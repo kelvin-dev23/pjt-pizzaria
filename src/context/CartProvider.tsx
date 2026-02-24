65421d6f57ab798212191fe7dfce2b57651cc55f
kelvin augusto <kelvinaugusto1021@gmail.com>
2026-02-24 13:50:22 -0300

    feat: persistencia do carrinho com localStorage

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -2,9 +2,22 @@ import { useState } from "react"
 import { CartContext } from "./CartContext"
 import type { Pizza } from "../types/Pizza"
 import type { CartItem } from "./CartContext"
+import { useEffect } from "react"
 
 export function CartProvider({ children }: { children: React.ReactNode }) {
-  const [carrinho, setCarrinho] = useState<CartItem[]>([])
+  const [carrinho, setCarrinho] = useState<CartItem[]>(() => {
+  const carrinhoSalvo = localStorage.getItem("carrinho")
+  
+
+  if (carrinhoSalvo) {
+    return JSON.parse(carrinhoSalvo)
+  }
+
+  return []
+})
+useEffect(() => {
+  localStorage.setItem("carrinho", JSON.stringify(carrinho))
+}, [carrinho])
   
   function removerDoCarrinho(id: number) {
   setCarrinho((prev) => prev.filter((item) => item.id !== id))
