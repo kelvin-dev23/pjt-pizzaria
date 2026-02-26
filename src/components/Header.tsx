3f3e0db2a7b7949e411e21b7f3025900c94da557
kelvin augusto <kelvinaugusto1021@gmail.com>
2026-02-26 17:09:50 -0300

    feat:adiciona exibição de usuário logado e funcionalidade de logout no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,58 @@
-import { useEffect, useState } from "react"
-import { estaAberto } from "../utils/horario"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { estaAberto } from "../utils/horario";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
-  const [aberto, setAberto] = useState(estaAberto())
+  const [aberto, setAberto] = useState(estaAberto());
+  const [authUser, setAuthUser] = useState<{ email: string } | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("authUser");
+
+    if (storedUser) {
+      setAuthUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   useEffect(() => {
     const intervalo = setInterval(() => {
-      setAberto(estaAberto())
-    }, 60000) // atualiza a cada 1 minuto
+      setAberto(estaAberto());
+    }, 60000);
 
-    return () => clearInterval(intervalo)
-  }, [])
+    return () => clearInterval(intervalo);
+  }, []);
+  function handleLogout() {
+    localStorage.removeItem("authUser");
+    navigate("/");
+    window.location.reload();
+  }
   return (
     <header className="bg-primary text-light shadow-md">
       <div className="flex items-center gap-4">
         <h1 className="text-xl font-bold">🍕 Casa da Pizza</h1>
+        <div className="flex items-center gap-4">
+          {authUser ? (
+            <>
+              <span className="text-sm">👤 {authUser.email}</span>
+
+              <button
+                onClick={handleLogout}
+                className="bg-secondary text-light px-4 py-2 rounded-lg font-bold"
+              >
+                Sair
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="bg-secondary text-light px-4 py-2 rounded-lg font-bold"
+            >
+              Entrar
+            </button>
+          )}
+        </div>
 
         {aberto ? (
           <span className="bg-green-500 px-3 py-1 rounded-full text-sm font-semibold">
@@ -34,5 +71,5 @@ export function Header() {
         <Link to="/carrinho">Carrinho</Link>
       </nav>
     </header>
-  )
+  );
 }
